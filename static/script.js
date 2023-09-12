857ae07bb5d26f57caa5c4e23dee586e8ac1a6d7
mkothm <mkothman.jr@gmail.com>
2023-09-12 17:01:07 +0100

    final corrections

--- static/script.js
+++ static/script.js
@@ -5,7 +5,6 @@ let notifyTime = 0;
 
 // Function to start the timer
 function startTimer() {
-    startTime = Date.now() - (notifyTime * 60 * 1000);
     timer = setInterval(updateTimer, 1000);
 }
 
@@ -18,6 +17,7 @@ function stopTimer() {
 function resetTimer() {
     clearInterval(timer);
     notifyTime = 0;
+    startTime = Date.now();
     document.getElementById('display').textContent = '00:00:00';
 }
 
@@ -31,9 +31,11 @@ function updateTimer() {
     const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes % 60).padStart(2, '0')}:${String(seconds % 60).padStart(2, '0')}`;
     document.getElementById('display').textContent = formattedTime;
 
-    if (notifyTime > 0 && minutes >= notifyTime) {
+    if (notifyTime > 0 && seconds >= notifyTime * 60) {
         alert(`Time's up! ${notifyTime} minutes have passed.`);
-        notifyTime = 0;
+        clearInterval(timer); // Stop the timer
+        startTime = Date.now(); // Reset the startTime
+        document.getElementById('display').textContent = '00:00:00';
     }
 }
 
@@ -47,7 +49,9 @@ document.getElementById('set-notify').addEventListener('click', function () {
     const inputTime = parseInt(document.getElementById('notify-time').value);
     if (!isNaN(inputTime) && inputTime >= 1) {
         notifyTime = inputTime;
+        startTime = Date.now(); // Start the timer from zero
         alert(`Notification set for ${notifyTime} minutes.`);
+        startTimer(); // Start the timer
     } else {
         alert('Invalid notification time. Please enter a positive integer.');
     }
@@ -56,5 +60,9 @@ document.getElementById('set-notify').addEventListener('click', function () {
 // Event listener for clearing notification time
 document.getElementById('clear-notify').addEventListener('click', function () {
     notifyTime = 0;
+    clearInterval(timer); // Stop the timer
+    startTime = Date.now(); // Reset the startTime
+    document.getElementById('display').textContent = '00:00:00';
+    document.getElementById('notify-time').value = '0'; // Reset the input field to 0
     alert('Notification cleared.');
 });
